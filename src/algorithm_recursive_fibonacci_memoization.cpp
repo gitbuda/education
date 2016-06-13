320f6fdcf5a25e0cb428fdb7ac445838a879a78d
gitbuda <mbudiselicbuda@gmail.com>
2016-06-13 13:22:39 +0200

    recursive fibonacci memoization range update

--- src/algorithm_recursive_fibonacci_memoization.cpp
+++ src/algorithm_recursive_fibonacci_memoization.cpp
@@ -3,7 +3,11 @@
 
 #include "include/using_iostream.hpp"
 
-auto fibonacci(int x, std::vector<int> &results)
+using index_type = int;
+using value_type = long long;
+
+template <typename index_type, typename value_type>
+value_type fibonacci(index_type x, std::vector<value_type> &results)
 {
     if (x == 1) return 0;
     if (x == 2) return 1;
@@ -18,11 +22,14 @@ int main(int argc, char **argv)
 {
     assert(argc == 2);
 
-    int index = std::atoi(argv[1]);
-    assert(index > 0);
+    index_type index = std::atoi(argv[1]);
 
-    std::vector<int> results(index + 1, -1);
-    auto result = fibonacci(index, results);
+    // index geater or equal 93 produces values which cann't fit
+    // in the long long range
+    assert(index > 0 && index < 94);
+    
+    std::vector<value_type> results(index + 1, -1);
+    auto result = fibonacci<index_type, value_type>(index, results);
     cout << result << endl;
 
     return 0;
