8a993cd6f352359a93c50b4177604c67b4bf2e89
Marko Budiselic <mbudiselicbuda@gmail.com>
2016-07-25 00:15:40 +0100

    C++17: fold expressions

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ for file_full_path in src/*.cpp; do
     file_full_name=$(basename $file_full_path)
     file_name="${file_full_name%.*}"
     extension="${file_full_name##*.}"
-    g++ -std=c++1y $file_full_path -o build/${file_name}.run
+    clang++-3.8 -std=c++1z $file_full_path -o build/${file_name}.run
 done
---
+++ src/fold_expressions.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+template <typename ... T>
+auto sum_old(T ... t)
+{
+    typename std::common_type<T...>::type result{};
+    (void)std::initializer_list<int>{ (result += t, 0)... };
+    return result;
+}
+
+template <typename ... T>
+auto sum(T ... t)
+{
+    return (t + ...);
+}
+
+template <typename ... T>
+auto div(T ... t)
+{
+    return (... / t);
+}
+
+template <typename ... T>
+auto avg(T ... t)
+{
+    return (t + ...) / sizeof...(t);
+}
+
+template <typename ... T>
+auto print(T&& ... t)
+{
+    (cout << ... << std::string(t) + " ") << endl;
+}
+
+template <typename ... T>
+auto all_true(T ... t)
+{
+    return ((t == true) && ...);
+}
+
+auto main() -> int
+{
+    cout << "C++11/14 sum: " << sum_old(1.0, 2, 3, 4, 5.0) << endl;
+
+    cout << "C++17 fold sum: " << sum(1.0, 2.0, 3, 4, 5) << endl;
+
+    cout << "C++17 fold div: " << div(1.0, 2.0, 3.0) << endl;
+
+    cout << "C++17 fold avg: " << avg(1.0, 2.0, 3.0) << endl;
+
+    print("This", "is", "a", "test!");
+
+    bool is_true = 2 == 10;
+    cout << "All true: " << all_true(true, true, is_true) << endl;
+
+    return 0;
+}
