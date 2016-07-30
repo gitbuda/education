234909306f0f3f255a11c8e151dae8194b1c0c19
Marko Budiselic <mbudiselicbuda@gmail.com>
2016-07-30 14:57:39 +0100

    std::hash experiment

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# TODO: CMake
+
 # compile all c++ files and place executables in the build folder
 for file_full_path in src/*.cpp; do
     file_full_name=$(basename $file_full_path)
     file_name="${file_full_name%.*}"
     extension="${file_full_name##*.}"
-    clang++-3.8 -std=c++1z $file_full_path -o build/${file_name}.run
+    clang++-3.8 -g -D_GLIBCXX_DEBUG -std=c++1z $file_full_path -o build/debug_${file_name}.run
 done
