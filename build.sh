a27ae56c5b70f361fdc88f68fd05ba84bac5d037
Marko Budiselic <mbudiselicbuda@gmail.com>
2016-08-14 05:13:25 +0100

    example with nested namespaces and std::clamp

--- build.sh
+++ build.sh
@@ -3,9 +3,13 @@
 # TODO: CMake
 
 # compile all c++ files and place executables in the build folder
-for file_full_path in src/*.cpp; do
+for file_full_path in src/nested_namespaces_std_clamp.cpp; do
     file_full_name=$(basename $file_full_path)
     file_name="${file_full_name%.*}"
     extension="${file_full_name##*.}"
-    clang++-3.8 -g -D_GLIBCXX_DEBUG -std=c++1z $file_full_path -o build/debug_${file_name}.run
+    # libstdc++ -> GNU C++ standard library
+    clang++-3.9 -g -D_GLIBCXX_DEBUG -std=c++1z \
+        $file_full_path -o build/debug_${file_name}.run \
+        -stdlib=libc++
+        # -stdlib=libstdc++
 done
