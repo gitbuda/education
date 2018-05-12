1cc7f1e67cf4b6c1d5e6abaa0873bb4f3f52f85d
Marko Budiselic <mbudiselicbuda@gmail.com>
2018-05-12 12:49:50 +0200

    Add first lisp macro example

--- lisp/02_cd_db.lisp
+++ lisp/02_cd_db.lisp
@@ -80,8 +80,58 @@
       (mapcar
         #'(lambda (row)
             (when (funcall selector-fn row)
-              (if title (setf (getf row :title) rating))
-              (if artist (setf (getf row :artist) rating))
+              (if title (setf (getf row :title) title))
+              (if artist (setf (getf row :artist) artist))
               (if rating (setf (getf row :rating) rating))
               (if ripped-p (setf (getf row :ripped) ripped)))
             row) *db*)))
+
+(defun delete-rows (selector-fn)
+  (setf *db* (remove-if selector-fn *db*)))
+
+(defmacro backwards (expr) (reverse expr))
+
+;;; (backwards ("hello world" t format)) --> (format t "hello world")
+
+;;; WHERE MACRO
+;;; (defun make-comparison-expr (field value)
+;;;  (list 'equal (list 'getf 'cd field) value))
+;;; In a back-quoted expression, any subexpression that’s preceded
+;;; by a comma is evaluated.
+(defun make-comparison-expr (field value)
+  `(equal (getf cd ,field) ,value))
+
+(defun make-comparisons-list (fields)
+  (loop while fields
+     collecting (make-comparison-expr (pop fields) (pop fields))))
+
+;;; With a &rest in its parameter list, a function or macro can
+;;; take an arbitrary number of arguments, which are collected
+;;; into a single list that becomes the value of the variable
+;;; whose name follows the &rest
+
+;;; ,@ "splices" the value of the following expression—which must
+;;; evaluate to a list
+
+;;; (macroexpand-1 '(macro ...)) -> macro expansion
+
+(defmacro where (&rest clauses)
+  `#'(lambda (cd) (and ,@(make-comparisons-list clauses))))
+;;; WHERE MACRO
+
+;;; UPDATE MACRO
+(defun make-update (field value)
+  `(setf (getf row ,field) ,value))
+
+(defun make-updates-list (fields)
+  (loop while fields
+     collecting (make-update (pop fields) (pop fields))))
+
+(defmacro update (selector-fn &rest updates)
+  `(setf *db*
+      (mapcar
+        #'(lambda (row)
+            (when (funcall ,selector-fn row)
+              ,@(make-updates-list updates))
+            row) *db*)))
+;;; UPDATE MACRO
