45231fa8e138b3865c06d84b748f3c8e92c818db
Marko Budiselic <mbudiselicbuda@gmail.com>
2018-08-07 07:40:45 +0200

    Add iterations into Linux vectorized I/O benchmark

--- linux/vectorized_io/main.cpp
+++ linux/vectorized_io/main.cpp
@@ -1,6 +1,8 @@
+#include <algorithm>
 #include <chrono>
 #include <cstring>
 #include <iostream>
+#include <vector>
 
 #include <unistd.h>
 
@@ -11,6 +13,11 @@ struct Profile {
   std::chrono::high_resolution_clock::time_point start;
   Profile(const std::string &name)
       : name(name), start(std::chrono::high_resolution_clock::now()) {}
+  auto Delta() {
+    using duration = std::chrono::duration<double>;
+    auto delta = std::chrono::high_resolution_clock::now() - start;
+    return std::chrono::duration_cast<duration>(delta).count();
+  }
   ~Profile() {
     using duration = std::chrono::duration<double>;
     auto delta = std::chrono::high_resolution_clock::now() - start;
@@ -20,48 +27,69 @@ struct Profile {
   }
 };
 
-// TODO: write with c primitives
+// TODO: write Profile that is going to be aware of iterations
 // TODO: take care of the flush before writing
 // TODO: write a proper benchmark
 // TODO: visualize the results
+// TODO: use smart pointers
 
-int main() {
-  char str[16384];
-  ssize_t nwritten;
-  for (int i = 0; i < 16383; ++i) str[i] = 'a';
-  str[16383] = '\n';
+auto profile_write(int length) {
+  std::vector<char> str(length, 'a');
   FILE *file = fopen("file", "a");
+  ssize_t nwritten;
+  double duration;
   {
-    Profile("write call");
-    nwritten = write(fileno(file), str, 16384);
+    Profile profile("write call");
+    nwritten = write(fileno(file), &str.front(), str.size());
+    duration = profile.Delta();
   }
   std::cout << nwritten << std::endl;
+  return duration;
+}
 
-  char str0[4096];
-  for (int i = 0; i < 4096; ++i) str0[i] = 'a';
-  char str1[4096];
-  for (int i = 0; i < 4096; ++i) str1[i] = 'a';
-  char str2[4096];
-  for (int i = 0; i < 4096; ++i) str2[i] = 'a';
-  char str3[4096];
-  for (int i = 0; i < 4095; ++i) str3[i] = 'a';
-  str3[4095] = '\n';
-  struct iovec iov[4];
+auto profile_writev(const std::vector<int> &lenghts) {
+  std::vector<struct iovec> buffers(lenghts.size());
+  for (auto i = 0; i < lenghts.size(); ++i) {
+    char *str = new char[lenghts[i]];
+    for (auto i = 0; i < lenghts[i]; ++i) str[i] = 'a';
+    buffers[i].iov_base = str;
+    buffers[i].iov_len = lenghts[i];
+  }
   ssize_t nwrittenv;
-  iov[0].iov_base = str0;
-  iov[0].iov_len = 4096;
-  iov[1].iov_base = str1;
-  iov[1].iov_len = 4096;
-  iov[2].iov_base = str2;
-  iov[2].iov_len = 4096;
-  iov[3].iov_base = str3;
-  iov[3].iov_len = 4096;
   FILE *filev = fopen("filev", "a");
+  double duration;
   {
-    Profile("writev call");
-    nwrittenv = writev(fileno(filev), iov, 4);
+    Profile profile("writev call");
+    nwrittenv = writev(fileno(filev), &buffers.front(), lenghts.size());
+    duration = profile.Delta();
   }
   std::cout << nwrittenv << std::endl;
+  for (int i = 0; i < lenghts.size(); ++i) {
+    delete[] static_cast<char *>(buffers[i].iov_base);
+  }
+  return duration;
+}
 
+template <typename T>
+auto median(T &v) {
+  size_t n = v.size() / 2;
+  nth_element(v.begin(), v.begin() + n, v.end());
+  return v[n];
+}
+
+int main() {
+  int niterations = 10;
+  int length = 65536;
+  std::vector<int> lenghts = {16384, 16384, 16384, 16384};
+  std::vector<double> write_durations;
+  std::vector<double> writev_durations;
+  for (int i = 0; i < niterations; ++i) {
+    write_durations.push_back(profile_write(length));
+    writev_durations.push_back(profile_writev(lenghts));
+  }
+  auto write_median = median(write_durations);
+  auto writev_median = median(writev_durations);
+  std::cout << "Write duration median: " << write_median << std::endl;
+  std::cout << "Writev duration median: " << writev_median << std::endl;
   return 0;
 }
