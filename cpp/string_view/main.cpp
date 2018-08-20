851ae5df43c84674c0ed46993b25516dbc20f405
Marko Budiselic <mbudiselicbuda@gmail.com>
2018-08-20 22:18:34 +0200

    Add string_view example

---
+++ cpp/string_view/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string_view>
+
+int main() {
+  char str_array[] = "   Test";
+  std::cout << str_array << std::endl;
+
+  std::string_view str_view(str_array, std::size(str_array));
+  std::cout << str_view << std::endl;
+
+  str_array[3] = 't';
+  std::cout << str_view << std::endl;
+
+  // Update only view.
+  str_view.remove_prefix(
+      std::min(str_view.find_first_not_of(" "), str_view.size()));
+  std::cout << str_view << std::endl;
+
+  std::cout << str_array << std::endl;
+
+  return 0;
+}
