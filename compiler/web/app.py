ec65668ba9ae00a1a8c46649c065282f0048eb23
Marko Budiselic <mbudiselicbuda@gmail.com>
2019-11-16 14:16:13 +0100

    Remove old dependencies from compiler project

--- compiler/web/app.py
+++ compiler/web/app.py
@@ -5,9 +5,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def root():
     return 'Compiler web interface'
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="8080")
