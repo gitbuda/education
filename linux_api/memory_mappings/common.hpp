a98502f7c1d6a85a21c73db700f6b801b147ee86
Marko Budiselić <mbudiselicbuda@gmail.com>
2020-04-04 22:18:00 +0200

    Add simple example of mmap (#7)
    * Add example how to communicate between 2 processes via mmap region

---
+++ linux_api/memory_mappings/common.hpp
@@ -0,0 +1,45 @@
+#include <cassert>
+#include <cstdlib>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+void Help(const std::string &message) {
+  std::cout << message << std::endl;
+  std::exit(1);
+}
+
+void ErrorExit(const std::string &message) {
+  std::cerr << message << std::endl;
+  std::exit(1);
+}
+
+void FileWrite(const std::filesystem::path &path,
+               const std::vector<std::uint8_t> &data) {
+  assert(data.size() > 0);
+  std::ofstream file(path, std::ios_base::binary);
+  assert(file.is_open());
+  file.write(reinterpret_cast<const char *>(data.data()),
+             data.size() * sizeof(data[0]));
+  file.close();
+}
+
+std::vector<std::uint8_t> FileRead(const std::filesystem::path &path) {
+  std::ifstream file(path, std::ios::in | std::ios::binary);
+  std::vector<std::uint8_t> data(
+      (std::istreambuf_iterator<char>(file)),
+      std::istreambuf_iterator<char>());
+  return data;
+}
+
+void MemoryWrite(void *start_addr, const std::vector<std::uint8_t> &data) {
+  assert(data.size() > 0);
+  std::memcpy(start_addr, data.data(), data.size() * sizeof(data[0]));
+}
+
+std::vector<std::uint8_t> MemoryRead(void *start_addr, std::size_t length) {
+  std::vector<uint8_t> data(length, '\n');
+  std::memcpy(data.data(), start_addr, length);
+  return data;
+}
---
+++ linux_api/memory_mappings/p1.cpp
@@ -0,0 +1,37 @@
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <cstring>
+#include <thread>
+
+#include "common.hpp"
+
+int main(int argc, char* argv[]) {
+  if (argc != 2 || std::strcmp(argv[1], "--help") == 0) {
+    Help("Usage ./program_name file_path");
+  }
+
+  std::vector<std::uint8_t> pid{'p', '1'};
+
+  std::filesystem::path file(argv[1]);
+  int fd = open(file.c_str(), O_RDWR);
+  if (fd == -1) {
+    ErrorExit("Failed to open file.");
+  }
+  void* addr =
+      mmap(NULL, getpagesize(), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if (addr == MAP_FAILED) {
+    ErrorExit("mmap failed.");
+  }
+  if (close(fd) == -1) {
+    ErrorExit("Failed to close file.");
+  }
+
+  MemoryWrite(addr, {'p', '2'});
+  if (msync(addr, 2, MS_SYNC) == -1) {
+    ErrorExit("Failed to open file.");
+  }
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+
+  return 0;
+}
