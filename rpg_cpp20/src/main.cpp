c3a08c75f71173516e73f662a129b57b078022c3
Marko Budiselic <mbudiselicbuda@gmail.com>
2020-04-18 18:46:49 +0200

    [rpg_cpp20] Part 3 - Handling Command Line Parameters

--- rpg_cpp20/src/main.cpp
+++ rpg_cpp20/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 
 #include <spdlog/spdlog.h>
 #include <imgui.h>
@@ -10,24 +11,63 @@
 
 #include <docopt/docopt.h>
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] const char **argv)
+static constexpr auto USAGE =
+  R"(C++ Weekly Game.
+
+    Usage:
+        game [options]
+
+    Options:
+        -h --help    Show this screen.
+        --width=N    Screen width in pixels [default: 1024].
+        --height=N   Screen height in pixels [default: 768].
+        --scale=N    Scaling factor [default: 2].
+)";
+
+int main(int argc, const char **argv)
 {
+  std::map<std::string, docopt::value> args = docopt::docopt(
+    USAGE, { std::next(argv), std::next(argv, argc) }, true, "Game 0.0");
+
+  const auto width = args["--width"].asLong();
+  const auto height = args["--height"].asLong();
+  const auto scale = args["--scale"].asLong();
+
+  if (width < 0 || height < 0 || scale < 1 || scale > 5) {
+    spdlog::error("Command line options are out of reasonable range.");
+    for (auto const &arg : args) {
+      if (arg.second.isString()) {
+        spdlog::info(
+          "Parameter set: {}='{}'", arg.first, arg.second.asString());
+      }
+    }
+    abort();
+  }
   // Use the default logger (stdout, multi-threaded, colored)
-  spdlog::info("Starting ImGui + SFML");
-  sf::VideoMode mode(1024, 760);
-  auto title = "ImGui + SFML = <3";
-  sf::RenderWindow window(mode, title);
+  sf::RenderWindow window(sf::VideoMode(static_cast<unsigned int>(width),
+                            static_cast<unsigned int>(height)),
+    "ImGui + SFML = <3");
   window.setFramerateLimit(60);
   ImGui::SFML::Init(window);
 
-  constexpr auto scale_factor = 2.0;
+  const auto scale_factor = static_cast<float>(scale);
   ImGui::GetStyle().ScaleAllSizes(scale_factor);
   ImGui::GetIO().FontGlobalScale = scale_factor;
 
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
-
-  std::array<bool, 13> states{ false };
+  constexpr std::array steps = { "The Plan",
+    "Getting Started",
+    "Finding Errors as Soon as Possible",
+    "Handling Command Line Parameters",
+    "C++ 20 So Far",
+    "Reading SFML Input States",
+    "Managing Game State",
+    "Making Our Game Testable",
+    "Making Game State Allocator Aware",
+    "Add Logging To Game Engine",
+    "Draw a Game Map",
+    "Dialog Trees",
+    "Porting From SFML to SDL" };
+  std::array<bool, steps.size()> states{ false };
 
   sf::Clock deltaClock;
   while (window.isOpen()) {
@@ -41,28 +81,14 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char **argv)
     // By doing this ImGui knows how much time has elapsed.
     ImGui::SFML::Update(window, deltaClock.restart());
     ImGui::Begin("The Plan");
-    int index = 0;
-    for (const auto &step : { "The Plan",
-           "Getting Started",
-           "Finding Errors as Soon as Possible",
-           "Handling Command Line Parameters",
-           "C++ 20 So Far",
-           "Reading SFML Input States",
-           "Managing Game State",
-           "Making Our Game Testable",
-           "Making Game State Allocator Aware",
-           "Add Logging To Game Engine",
-           "Draw a Game Map",
-           "Dialog Trees",
-           "Porting From SFML to SDL" }) {
-      ImGui::Checkbox(fmt::format("{} : {}", index, step).c_str(),
-        std::next(std::begin(states), index));
+    for (size_t index = 0; const auto &step : steps) {
+      ImGui::Checkbox(
+        fmt::format("{} : {}", index, step).c_str(), &states.at(index));
       index++;
     }
     ImGui::End();
 
     window.clear();
-    window.draw(shape);
     ImGui::SFML::Render(window);
     window.display();
   }
