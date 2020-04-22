2b23b29a6237e735cae0d290c0c2d1c24f182efb
Marko Budiselic <mbudiselicbuda@gmail.com>
2020-04-22 23:12:41 +0200

    [pl/cpp] Move variant to cpp learning project

--- programming_languages/cpp/project/src/variant.cpp
+++ programming_languages/cpp/project/src/variant.cpp
@@ -1,8 +1,6 @@
 // C++ Reference std::variant
 //
 // https://en.cppreference.com/w/cpp/utility/variant
-//
-// Compile command: clang++ -std=c++1z main.cpp
 #include <iomanip>
 #include <iostream>
 #include <memory>
@@ -12,13 +10,13 @@
 
 // Helper function to print any number of arguments of any type with the
 // possibility to define indentation.
-template <int level = 1, int indent = 2, typename... T>
+template<int level = 1, int indent = 2, typename... T>
 auto print(T &&... t) {
   std::cout << std::string(indent * level, ' ');
   ((std::cout << t), ...);
 }
 auto println() { std::cout << std::endl; }
-template <int level = 1, int indent = 2, typename... T>
+template<int level = 1, int indent = 2, typename... T>
 auto println(T &&... t) {
   print<level, indent>(t...);
   println();
@@ -26,16 +24,17 @@ auto println(T &&... t) {
 
 struct Struct {
   Struct() { println("Struct default constructor called."); }
-  Struct(const Struct &) { println("Struct copy constructor called."); }
-  Struct(Struct &&) { println("Struct move constructor called."); }
-  Struct &operator=(const Struct &) {
+  Struct([[maybe_unused]] const Struct &other) { println("Struct copy constructor called."); }
+  Struct([[maybe_unused]] Struct &&other) noexcept { println("Struct move constructor called."); }
+  Struct &operator=([[maybe_unused]] const Struct &other) {
     println("Struct copy assignment called.");
     return *this;
   }
-  Struct &operator=(Struct &&) {
+  Struct &operator=([[maybe_unused]] Struct &&other) noexcept {
     println("Struct move assignment called.");
     return *this;
   }
+  ~Struct() {}
   operator int() { return 2000; }
 };
 // Required to test variadic initialization with an error.
@@ -45,12 +44,12 @@ struct ErrorStruct {
 
 // Helper visitor construct explained in depth here
 // https://dev.to/tmr232/that-overloaded-trick-overloading-lambdas-in-c17.
-template <class... Ts>
+template<class... Ts>
 struct overloaded : Ts... {
   using Ts::operator()...;
 };
-template <class... Ts>
-overloaded(Ts...)->overloaded<Ts...>;
+template<class... Ts>
+overloaded(Ts...) -> overloaded<Ts...>;
 
 int main() {
   // Basic usage example.
@@ -61,16 +60,14 @@ int main() {
   println("Get by 0 index: ", std::get<0>(basic));
   println("Index: ", basic.index());
 
-  basic = 10.5f;
+  basic = 10.5F;
   println("Get by float type: ", std::get<float>(basic));
   println("Get by 1 index: ", std::get<1>(basic));
   println("Index: ", basic.index());
 
   try {
     std::get<0>(basic);
-  } catch (std::bad_variant_access &) {
-    println("Bad variant access");
-  }
+  } catch (std::bad_variant_access &) { println("Bad variant access"); }
   println();
 
   // Visit example.
@@ -82,8 +79,8 @@ int main() {
   // Get pointer example.
   println<0>("Get pointer example");
   std::variant<int, float> ptr_example;
-  ptr_example = 1.f;
-  if (auto ptr = std::get_if<float>(&ptr_example)) {
+  ptr_example = 1.F;
+  if (auto *ptr = std::get_if<float>(&ptr_example)) {
     println("Variant value based on pointer: ", *ptr);
   } else {
     println("Failed to get value");
@@ -92,20 +89,17 @@ int main() {
 
   // Valueless example.
   println<0>("Valueless example");
-  std::variant<float, int> valueless{0.f};
+  std::variant<float, int> valueless{0.F};
   try {
     valueless.emplace<int>(Struct());
     valueless.emplace<1>(ErrorStruct());
-  } catch (int e) {
-    println("Error during variant emplace operation");
-  }
+  } catch (int e) { println("Error during variant emplace operation"); }
   println(std::boolalpha, "Valueless: ", valueless.valueless_by_exception());
   println();
 
   // Overloaded pattern.
   println<0>("Overloaded example pattern");
-  std::vector<std::variant<int, long, double, std::string>> vec = {10, 15l, 1.5,
-                                                                   "hello"};
+  std::vector<std::variant<int, long, double, std::string>> vec = {10, 15l, 1.5, "hello"};
   print(""); // Just indentation.
   for (auto &v : vec) {
     std::visit(
@@ -113,9 +107,7 @@ int main() {
             [](int arg) { print<0>("int: ", arg, ", "); },
             [](long arg) { print<0>("long: ", arg, ", "); },
             [](double arg) { print<0>("double: ", std::fixed, arg, ", "); },
-            [](const std::string &arg) {
-              print<0>("std::string: ", std::quoted(arg));
-            },
+            [](const std::string &arg) { print<0>("std::string: ", std::quoted(arg)); },
         },
         v);
   }
