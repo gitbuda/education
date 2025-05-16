2285f3350f3953d78ccd3559bc8797b123a94b4e
Marko Budiselic <mbudiselicbuda@gmail.com>
2025-05-16 19:19:01 +0000

    Add initialization of distributed pytorch

--- ai/llm/init.bash
+++ ai/llm/init.bash
@@ -2,19 +2,11 @@
 
 sudo apt install python3-pip python3-virtualenv
 
-# TODO: set and activate the virtualenv
-
-# Install stable version of PyTorch libraries using pip
-pip install torch torchvision torchao
-# # Or nightly install for latest features
-# pip install --pre torch torchvision torchao --index-url https://download.pytorch.org/whl/nightly/cu126 # full options are cpu/cu118/cu121/cu124/cu126/xpu
-pip install torchtune
-
-# Download DeepSeek-V3 https://huggingface.co/deepseek-ai/DeepSeek-V3-Base#61-inference-with-deepseek-infer-demo-example-only
-# rm -rf DeepSeek-V3
-if [ ! -d DeepSeek-V3 ]; then
-  git clone https://github.com/deepseek-ai/DeepSeek-V3.git
-fi
+# # Download DeepSeek-V3 https://huggingface.co/deepseek-ai/DeepSeek-V3-Base#61-inference-with-deepseek-infer-demo-example-only
+# # rm -rf DeepSeek-V3
+# if [ ! -d DeepSeek-V3 ]; then
+#   git clone https://github.com/deepseek-ai/DeepSeek-V3.git
+# fi
 
 # tune download org/model --output-dir /tmp/model
 if [ ! -d DeepSeek-R1-Distill-Qwen-1.5B ]; then
