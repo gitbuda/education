3b21a3be4d4ab7621abb6dd59a34b43b8a3e1961
Marko Budiselic <mbudiselicbuda@gmail.com>
2025-05-16 10:15:48 -0700

    Dataset is there

--- ai/llm/main.py
+++ ai/llm/main.py
@@ -1,12 +1,20 @@
 # 1. tokenizer        DONE
 # 2. model            DONE
 # 3. run example      DONE
-# 4. generate dataset NEXT
-# 5. single GPU
-# 6. multi-GPU
+# 4. generate dataset DONE
+# 5. single GPU train NEXT
+# 6. multi GPU
 
+from datasets import load_dataset, config
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
+
+# Login using e.g. `huggingface-cli login` to access this dataset.
+print("datasets are cached under: ", config.HF_DATASETS_CACHE)
+text2cypher_2025v1 = "neo4j/text2cypher-2025v1"
+ds = load_dataset(text2cypher_2025v1)
+print(ds)
+print(ds['train'][0]["cypher"])
 
 # model_path = "/tmp/Qwen2-0.5B-Instruct"
 model_path = "/tmp/DeepSeek-R1-Distill-Qwen-1.5B"
@@ -25,8 +33,6 @@ model.to(device)
 # Example inference.
 input_text = "Your input text here"
 input_ids = tokenizer.encode(input_text, return_tensors="pt").to(device)
-
-# Generate text
 with torch.no_grad():
     output = model.generate(input_ids, max_length=50)
     generated_text = tokenizer.decode(output[0])
